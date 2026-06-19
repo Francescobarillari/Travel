805935b0212829f5ae0fa32cee2943866ea3d98a
ValeSta0 <valeriostasyuk123@gmail.com>
2026-06-19 15:02:50 +0200

    Correzione e salvataggio dei file Activity

---
+++ backend/src/main/java/it/unical/ea/Travel/DTOs/activity/ActivityDto.java
@@ -0,0 +1,18 @@
+package it.unical.ea.Travel.DTOs.activity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ActivityDto {
+    private UUID id;
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+}
